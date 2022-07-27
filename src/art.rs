b8c4ead4bcfb4d7d2da65a319112fc74bd97d7cf
Lina <61861965+l1na-forever@users.noreply.github.com>
2022-07-26 23:10:24 -0700

    support file:// urls, resize images to thumbnail size before sending

--- src/art.rs
+++ src/art.rs
@@ -1,11 +1,14 @@
 use crate::configuration::Configuration;
 use crate::notifier::NotificationImage;
 use image::io::Reader as ImageReader;
+use std::fs;
 use std::io::{Cursor, Read};
 use std::time::Duration;
 use thiserror::Error;
+use url::Url;
 
 const ART_SIZE_LIMIT: usize = 5_000_000; // ~5MB download size limit
+const THUMBNAIL_SIZE: u32 = 256; // generate <size> * <size> notification icons
 
 #[derive(Debug, Error)]
 pub enum ArtFetcherError {
@@ -17,6 +20,9 @@ pub enum ArtFetcherError {
 
     #[error("error decoding image")]
     Decode(#[from] image::ImageError),
+
+    #[error("error parsing URL")]
+    Location(#[from] url::ParseError),
 
     #[error("invalid response")]
     Invalid(),
@@ -34,10 +40,16 @@ impl ArtFetcher {
     }
 
     pub fn get_album_art(&self, url: &str) -> Result<NotificationImage, ArtFetcherError> {
-        let body = self.fetch_url(url)?;
+        // Determine if we have a local file:// or remote http(s):// URL
+        let parsed_url = Url::parse(url)?;
+        let body: Vec<u8> = match parsed_url.scheme() {
+            "file" => fs::read(parsed_url.to_file_path().unwrap())?,
+            _ => self.fetch_url(url)?,
+        };
         let image = ImageReader::new(Cursor::new(body))
             .with_guessed_format()?
-            .decode()?;
+            .decode()?
+            .thumbnail(THUMBNAIL_SIZE, THUMBNAIL_SIZE);
         Ok(NotificationImage::from(image))
     }
 
